0372c04b894efaf2f217e42cecc14244e046db0a
Ahmed Abdulrahman <a.kasapbashi@live.com>
2020-08-23 17:26:36 +0200

    :sparkles: [TabBar] implement Tabs onLayout functionality: - Measure each Tab width and get it's offset - Calculate absolute page offset - Get scrollX position

--- src/TabBar.js
+++ src/TabBar.js
@@ -10,22 +10,84 @@ import {
 const { width: wWidth } = Dimensions.get('window');
 
 const TabBar = React.memo(props => {
-  const { currentIndex, renderTab, onPress, sections } = props;
+  const { currentIndex, renderTab, onPress, sections, tabBarStyle } = props;
+
+  const [tabsMeasurements, setTabsMeasurements] = React.useState({});
+
+  const [
+    tabContainerMeasurements,
+    setTabContainerMeasurements
+  ] = React.useState({});
+
+  const scrollViewRef = React.useRef();
+
+  React.useEffect(() => {
+    if (scrollViewRef.current) {
+      const pageOffset = 0;
+
+      const containerWidth = wWidth;
+      const tabWidth = tabsMeasurements[currentIndex]?.width;
+      const nextTabMeasurements = tabsMeasurements[currentIndex + 1];
+      const nextTabWidth = nextTabMeasurements?.width || 0;
+      const tabOffset = tabsMeasurements[currentIndex]?.left;
+      const absolutePageOffset = pageOffset * tabWidth;
+      let newScrollX = tabOffset + absolutePageOffset;
+
+      newScrollX -=
+        (containerWidth -
+          (1 - pageOffset) * tabWidth -
+          pageOffset * nextTabWidth) /
+        2;
+      newScrollX = newScrollX >= 0 ? newScrollX : 0;
+
+      const rightBoundScroll = Math.max(
+        tabContainerMeasurements.width - containerWidth,
+        0
+      );
+
+      newScrollX =
+        newScrollX > rightBoundScroll ? rightBoundScroll : newScrollX;
+
+      scrollViewRef.current.scrollTo({
+        x: newScrollX,
+        animated: true
+      });
+    }
+  }, [currentIndex]);
+
+  const onTabContainerLayout = e => {
+    setTabContainerMeasurements(e.nativeEvent.layout);
+  };
+
+  const onTabLayout = key => ev => {
+    const { x, width, height } = ev.nativeEvent.layout;
+    setTabsMeasurements(prevState => ({
+      ...prevState,
+      [key]: {
+        left: x,
+        right: x + width,
+        width,
+        height
+      }
+    }));
+  };
 
   return (
-    <View style={[{ width: wWidth, position: 'relative' }]}>
+    <View style={[styles.scrollViewContainer, tabBarStyle]}>
       <ScrollView
         contentContainerStyle={styles.contentContainerStyle}
         horizontal
+        ref={scrollViewRef}
         showsHorizontalScrollIndicator={false}
       >
-        <View style={styles.sectionContainer}>
+        <View onLayout={onTabContainerLayout} style={styles.sectionContainer}>
           {sections.map((section, key) => {
             const isActive = currentIndex === key;
             return (
               <TouchableOpacity
-                onPress={() => onPress(key)}
                 key={`${section.title}-${key}`}
+                onPress={() => onPress(key)}
+                onLayout={e => onTabLayout(key)(e)}
               >
                 {renderTab({ isActive, ...section })}
               </TouchableOpacity>
@@ -38,6 +100,7 @@ const TabBar = React.memo(props => {
 });
 
 const styles = StyleSheet.create({
+  scrollViewContainer: { width: wWidth, position: 'relative' },
   contentContainerStyle: { flexDirection: 'row' },
   sectionContainer: {
     flexDirection: 'row'
